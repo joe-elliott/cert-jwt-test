aa010294af145a3d0e9df67edf8bb6bcf0eebf32
Joe Elliott <number101010@gmail.com>
2020-04-29 13:23:48 -0400

    Basic parsing
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
-	fmt.Println("let's goooo!")
+	rootCert := mustLoadCert("./crt/root.crt")
+	fmt.Println(rootCert.Subject.CommonName)
+}
+
+func mustLoadCert(f string) *x509.Certificate {
+	b, err := ioutil.ReadFile(f)
+	if err != nil {
+		panic(err)
+	}
+
+	block, _ := pem.Decode(b)
+	if block == nil {
+		panic("block is nil")
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return cert
 }
